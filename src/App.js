71817ba8554cb7964e283dfaad5a76715e8206ab
mimoon5860 <mahmodulislammoon@gmail.com>
2021-12-02 03:48:36 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState("");
+  const [cities, setCities] = useState([]);
+
+
+  const handleSubmit = (e) => {
+    setCities([...cities, city]);
+    e.target.reset();
+    e.preventDefault();
+  }
+  const removeCity = (cityName) => {
+    const rest = cities.filter(city => city !== cityName);
+    setCities(rest);
+  }
+
+  const updateCity = (cityName, newName) => {
+    const index = cities.indexOf(cityName);
+    const nowCities = [...cities];
+    nowCities[index] = newName;
+    setCities(nowCities);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input onChange={(e) => setCity(e.target.value)} type="text" />
+          <input type="submit" />
+        </form>
+      </div>
+      <div>
+        {
+          cities.map(city => <ShowWeather removeCity={removeCity} updateCity={updateCity} name={city} key={city} />)
+        }
+      </div>
     </div>
   );
+}
+
+
+const ShowWeather = ({ name, removeCity, updateCity }) => {
+  const [update, setUpdate] = useState(false);
+  const [weather, setWeather] = useState({});
+  const [newCity, setNewCity] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=c3bdefc76f6499029863ba4a34316e9c`;
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setWeather(data))
+  }, [url])
+
+
+  return (
+    <div>
+      <p>{name}</p>
+      <small>{(weather?.main?.temp - 273.15).toFixed(2)} &deg; Celsius</small>
+      <button onClick={() => removeCity(name)}>Remove</button>
+      <button onClick={() => setUpdate(true)}>Update</button>
+
+      {
+        update && <div>
+          <input required onChange={(e) => setNewCity(e.target.value)} type="text" />
+          <button onClick={() => updateCity(name, newCity)}>Update</button>
+        </div>
+      }
+      <br />
+      <br />
+    </div>
+  )
 }
 
 export default App;
